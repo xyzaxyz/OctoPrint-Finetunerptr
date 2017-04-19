40285e6589fad5fd1e9daa1e3a0415008b5d4513
xyzaxyz <andreas.bruckmann@gmx.net>
2017-04-19 22:50:19 +0200

    Up

--- octoprint_finetunerptr/__init__.py
+++ octoprint_finetunerptr/__init__.py
@@ -18,7 +18,7 @@ class FineTuneRptrPlugin(octoprint.plugin.AssetPlugin,
         )
 
     def get_template_configs(self):
-        return [ dict(type="tab", template="tab.html.j2", custom_bindings=True) ]
+        return [ dict(type="tab", template="tab.jinja2", custom_bindings=True) ]
 
     def get_update_information(self):
     	return dict(
@@ -33,7 +33,7 @@ class FineTuneRptrPlugin(octoprint.plugin.AssetPlugin,
             )
         )
 
-__plugin_name__ = "TuneRptr Helper"
+__plugin_name__ = "TuneRptr"
 
 def __plugin_load__():
 	global __plugin_implementation__
