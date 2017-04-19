d3524b744686abe5e955100b1c7a39aaf2736c2d
xyzaxyz <andreas.bruckmann@gmx.net>
2017-04-19 23:23:55 +0200

    075

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ plugin_package = "octoprint_finetunerptr"
 plugin_name = "OctoPrint-Finetunerptr"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.7"
+plugin_version = "0.75"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = """"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Andreas Bruckmann"
