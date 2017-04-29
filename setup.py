095f93457e8061f0a77d5d5fe8bbaa829a06f2f1
xyzaxyz <andreas.bruckmann@gmx.net>
2017-04-29 12:53:54 +0200

    Version 0.76 Release

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "octoprint_finetunerptr"
+plugin_identifier = "FineTuneRptr"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_finetunerptr"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Finetunerptr"
+plugin_name = "Octoprint_FineTuneRptr"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.75"
+plugin_version = "0.76"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
