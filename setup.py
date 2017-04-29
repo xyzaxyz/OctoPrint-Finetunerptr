a7964b1907a5b718ed61cb60ff1072f885861f44
xyzaxyz <andreas.bruckmann@gmx.net>
2017-04-29 13:03:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "FineTuneRptr"
+plugin_identifier = "octoprint_finetunerptr"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_finetunerptr"
